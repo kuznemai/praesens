f34ce62ba7f74e63e3fc1182f871b6c311a08557
Marina <marinakuznetsova420@gmail.com>
2025-08-01 15:51:13 +0300

    config fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
-
 export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+
   modules: [
     '@nuxt/ui'
   ],
+
   typescript: {
     strict: true,
   },
+
   app: {
+    baseURL: '/praesens/',
     head: {
       title: 'Praesens',
       meta: [
