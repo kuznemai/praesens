794b3ece1483317396035246a914dcc31cc78d01
Marina <marinakuznetsova420@gmail.com>
2025-09-23 14:11:23 +0300

    nuxt config bugfix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,14 @@
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/ui'
-  ],
+  modules: ['@nuxt/ui'],
   typescript: {
     strict: true,
   },
   app: {
     head: {
       title: 'Praesens',
-      meta: [
-        { name: 'description', content: 'Clone of praesens.ru' }
-      ]
-    }
-  }
+      meta: [{ name: 'description', content: 'Clone of praesens.ru' }],
+    },
+  },
 })
